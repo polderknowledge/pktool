551b29daba8174953b6a9b4d4b80a7d95eabf9fd
Jaapio <jaap@ijaap.nl>
2016-11-11 21:54:04 +0100

    Enables parsin dotfiles
    When processing a .travis file for example dot files shouldn't be ignored
    which they are, by default.

--- src/Command/CreateZFApplication.php
+++ src/Command/CreateZFApplication.php
@@ -83,7 +83,7 @@ final class CreateZFApplication extends AbstractCommand
         $this->updateLicenseFile('LICENSE.md', $values['license']);
 
         $finder = new Finder();
-        $finder->files()->in(getcwd());
+        $finder->files()->ignoreDotFiles(false)->in(getcwd());
 
         foreach ($finder as $file) {
             $this->replaceVariablesInFile($values, $file->getRealPath());
